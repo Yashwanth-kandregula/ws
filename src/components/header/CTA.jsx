1bf56a6753eb7c2b2488e322b17a3c803fe4ee3c
Yashwanth-kandregula <103552890+Yashwanth-kandregula@users.noreply.github.com>
2024-08-08 12:03:26 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CV from '../../assets/yashsinglepage(3).pdf';
+import CV from '../../src/assets/yashsinglepage.130c24003a89bd8120ef (6).pdf';
 
 const CTA = () => {
   return (
